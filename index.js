e41fa9cbe4745811c972399454efcfb5de671ce0
aditya-msl <aditya.jain@mittalsoftwarelabs.com>
2024-01-17 08:00:51 +0530

    commit

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 
-const cloudinary = require("cloudinary").v2;
-cloudinary.config(
-    { 
-        cloud_name: process.env.CLOUD_NAME, 
-        api_key: process.env.CLOUD_API_KEY, 
-        api_secret: process.env.CLOUD_API_SECRET, 
-    }
-);
-
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
